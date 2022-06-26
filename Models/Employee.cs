1520dbd76db88d2f96a7e48750d3a74bb26d87b3
Mohammed Azarudeen J <mohameedazarudeen.j@ivygdcad.ivy-support.com>
2022-06-26 22:02:20 +0530

    file upload

---
+++ Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FileuploadMVC.Models
+{
+    public class Employee
+    {
+     
+
+ 
+        public HttpPostedFileBase Resume { get; set; }
+        public string Skills { get; internal set; }
+    }
+
+}
